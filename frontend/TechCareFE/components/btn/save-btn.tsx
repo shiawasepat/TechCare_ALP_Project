fb4043974447ef99ed3f916ebbe594d5d9111dfc
rakhaazz18 <pakidadi83@gmail.com>
2026-05-11 02:17:30 +0800

    setup location utils and resolve missing dependencies

--- frontend/TechCareFE/components/btn/save-btn.tsx
+++ frontend/TechCareFE/components/btn/save-btn.tsx
@@ -1,6 +1,5 @@
-import { colors } from "@/styles/colors";
 import { Image } from "react-native";
 
 export function SaveBtn() {
-  return <Image source={require("@/assets/icons/bookmark-off.png")} style={[{ width: 24, height: 24, marginRight: 16 }, { tintColor: colors.tintIcon.tintColor }]} />;
+  return <Image source={require("@/assets/icons/bookmark-off.png")} style={[{ width: 24, height: 24, marginRight: 16 }, { tintColor: "#111827" }]} />;
 }
